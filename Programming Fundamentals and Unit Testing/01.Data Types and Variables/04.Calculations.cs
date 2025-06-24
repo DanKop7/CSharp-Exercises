6a801decd4fdb9d03b51d07631d5905649fadef4
DanKop7 <dk_reg@abv.bg>
2025-06-24 21:08:48 +0300

    Create 04.Calculations.cs

---
+++ Programming Fundamentals and Unit Testing/01.Data Types and Variables/04.Calculations.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(5 + 3 * 2);
+        Console.WriteLine(4 * (2 + 3));
+        Console.WriteLine((2 + 5) * (8 - 2) / 7);
+    }
+}
