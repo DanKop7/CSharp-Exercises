46ad9c5ad2f0fa1c68f5bf4ca80f6ccacde8683d
DanKop7 <dk_reg@abv.bg>
2025-07-19 14:19:14 +0300

    Create 08.Town Info.cs

---
+++ Programming Fundamentals and Unit Testing/01.Data Types and Variables/08.Town Info.cs
@@ -0,0 +1,12 @@
+using System;
+
+class TownInfo
+{
+    static void Main()
+    {
+        string townName = Console.ReadLine();
+        string country = Console.ReadLine();
+
+        Console.WriteLine($"{townName} is in {country}");
+    }
+}
