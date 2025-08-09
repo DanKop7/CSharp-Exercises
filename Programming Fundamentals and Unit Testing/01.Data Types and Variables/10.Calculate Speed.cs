42f42f7f8a1116498362c3ecf6461c484e0c2f0a
DanKop7 <dk_reg@abv.bg>
2025-08-09 20:46:15 +0300

    Create 10.Calculate Speed.cs

---
+++ Programming Fundamentals and Unit Testing/01.Data Types and Variables/10.Calculate Speed.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        double distance = double.Parse(Console.ReadLine());
+        double time = double.Parse(Console.ReadLine());
+
+        double speed = distance / time;
+
+        Console.WriteLine($"{speed:F2}");
+    }
+}
