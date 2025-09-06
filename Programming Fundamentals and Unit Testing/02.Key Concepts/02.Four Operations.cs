0e26cf5391e2683a8c1f3866a51c651377ef5211
DanKop7 <dk_reg@abv.bg>
2025-09-06 16:20:56 +0300

    Create 02.Four Operations.cs

---
+++ Programming Fundamentals and Unit Testing/02.Key Concepts/02.Four Operations.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        double firstNumber = double.Parse(Console.ReadLine());
+        double secondNumber = double.Parse(Console.ReadLine());
+
+        double sum = firstNumber + secondNumber;
+        double diff = firstNumber - secondNumber;
+        double product = firstNumber * secondNumber;
+        double div = firstNumber / secondNumber;
+
+        Console.WriteLine($"{firstNumber:F2} + {secondNumber:F2} = {sum:F2}");
+        Console.WriteLine($"{firstNumber:F2} - {secondNumber:F2} = {diff:F2}");
+        Console.WriteLine($"{firstNumber:F2} * {secondNumber:F2} = {product:F2}");
+        Console.WriteLine($"{firstNumber:F2} / {secondNumber:F2} = {div:F2}");
+    }
+}
