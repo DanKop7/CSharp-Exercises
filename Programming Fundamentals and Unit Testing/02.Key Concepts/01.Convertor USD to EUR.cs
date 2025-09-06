6fd1cf19d1d5eb9d1a9c607b2e0924c6591ebac1
DanKop7 <dk_reg@abv.bg>
2025-09-06 14:18:32 +0300

    Create 01.Convertor USD to EUR.cs

---
+++ Programming Fundamentals and Unit Testing/02.Key Concepts/01.Convertor USD to EUR.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        double dollar = double.Parse(Console.ReadLine());
+        double euro = dollar * 0.88;
+
+        Console.WriteLine($"{euro:F2}");
+    }
+}
