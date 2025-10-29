d0b5ca4aca9a5ea459561022760a5ea851e53ef8
DanKop7 <dk_reg@abv.bg>
2025-10-29 20:01:49 +0200

    Add program to reverse and display three numbers

---
+++ Programming Fundamentals and Unit Testing/03.Data Types, Variables And Key Concepts/01.Reversed Numbers.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        double firstNumber = double.Parse(Console.ReadLine());
+        double secondNumber = double.Parse(Console.ReadLine());
+        double thirdNumber = double.Parse(Console.ReadLine());
+
+        Console.WriteLine(thirdNumber);
+        Console.WriteLine(secondNumber);
+        Console.WriteLine(firstNumber);
+    }
+}
