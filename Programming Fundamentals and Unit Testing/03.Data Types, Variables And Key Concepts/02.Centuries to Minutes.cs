12e5ddec836349497e6ce1191f68ef695f23e78d
DanKop7 <dk_reg@abv.bg>
2025-11-02 15:03:56 +0200

    Add program to convert centuries to minutes

---
+++ Programming Fundamentals and Unit Testing/03.Data Types, Variables And Key Concepts/02.Centuries to Minutes.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int centuries = int.Parse(Console.ReadLine());
+
+        int centuriesToYears = centuries * 100;
+        int yearsToDays = (int)(centuriesToYears * 365.2422);
+        int daysToHours = yearsToDays * 24;
+        int hoursToMinutes = daysToHours * 60;
+
+        Console.WriteLine($"{centuries} centuries = {centuriesToYears} years = {yearsToDays} days = {daysToHours} hours = {hoursToMinutes} minutes");
+    }
+}
