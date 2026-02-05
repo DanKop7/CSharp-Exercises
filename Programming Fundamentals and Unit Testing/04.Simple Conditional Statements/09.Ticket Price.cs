dbdbb1968bff8d143680e9f469680ae3ec20bf9e
DanKop7 <dk_reg@abv.bg>
2026-02-05 19:04:38 +0200

    Add triangle validity check based on side lengths

---
+++ Programming Fundamentals and Unit Testing/04.Simple Conditional Statements/09.Ticket Price.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int c = int.Parse(Console.ReadLine());
+
+        if (a + b > c && a + c > b && b + c > a)
+        {
+            Console.WriteLine("Valid Triangle");
+        }
+        else
+        {
+            Console.WriteLine("Invalid Triangle");
+        }
+    }
+}
