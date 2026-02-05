008dd18279eb68b10f94b135813137faac112561
DanKop7 <dk_reg@abv.bg>
2026-02-05 18:30:21 +0200

    Add ticket price determination based on type

---
+++ Programming Fundamentals and Unit Testing/04.Simple Conditional Statements/08.Ticket Price.cs
@@ -0,0 +1,22 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string ticketType = Console.ReadLine();
+
+        if (ticketType == "student")
+        {
+            Console.WriteLine("$1.00");
+        }
+        else if (ticketType == "regular")
+        {
+            Console.WriteLine("$1.60");
+        }
+        else
+        {
+            Console.WriteLine("Invalid ticket type!");
+        }
+    }
+}
