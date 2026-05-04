9a0a8ae976fc77a56d356392b0d5e1f3231d5c84
DanKop7 <dk_reg@abv.bg>
2026-05-04 14:19:26 +0300

    Add Number Type classification program

---
+++ Programming Fundamentals and Unit Testing/06.Conditional Statements/01.Number Type.cs
@@ -0,0 +1,22 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+
+        if (number < 0)
+        {
+            Console.WriteLine("negative");
+        }
+        else if (number > 0)
+        {
+            Console.WriteLine("positive");
+        }
+        else
+        {
+            Console.WriteLine("zero");
+        }
+    }
+}
