7340c99acf1bf40fa9f7d5eade02570e5518888b
DanKop7 <dk_reg@abv.bg>
2026-05-05 14:34:56 +0300

    Add number operations with basic arithmetic functions

---
+++ Programming Fundamentals and Unit Testing/06.Conditional Statements/04.Number Operations.cs
@@ -0,0 +1,31 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        double firstNumber = double.Parse(Console.ReadLine());
+        double secondNumber = double.Parse(Console.ReadLine());
+        string mathOperator = Console.ReadLine();
+
+        double result = 0;
+
+        switch (mathOperator)
+        {
+            case "+":
+                result = firstNumber + secondNumber; 
+                break;
+            case "-":
+                result = firstNumber - secondNumber; 
+                break;
+            case "*":
+                result = firstNumber * secondNumber;
+                break;
+            case "/":
+                result = firstNumber / secondNumber; 
+                break;
+        }
+
+        Console.WriteLine($"{firstNumber} {mathOperator} {secondNumber} = {result:F2}");
+    }
+}
